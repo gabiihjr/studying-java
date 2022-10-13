f7136e30746b9df22f80e58b9073e1ab175bfe7c
Gabriela Hermenegildo Junior <gabriela.hermenegil@meta.com.br>
2022-10-13 16:36:52 -0300

    studying-java

---
+++ untitled1/src/TestingVariables.java
@@ -0,0 +1,10 @@
+public class TestingVariables {
+    public static void main(String[] args) {
+        System.out.println("hello new test");
+
+        int age;
+        age = 37;
+
+        System.out.println(age);
+    }
+}
