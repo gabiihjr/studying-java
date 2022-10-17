a36124107d3bba74ec53d52048d7589c2a77b9e5
Gabriela Hermenegildo Junior <gabriela.hermenegil@meta.com.br>
2022-10-17 13:25:54 -0300

    creating abstract classes

--- bytebank/src/CheckingAccount.java
+++ bytebank/src/CheckingAccount.java
@@ -4,4 +4,11 @@ public class CheckingAccount extends Account{
         super();
         //traz o construtor da classe mãe
     }
+
+    @Override
+    // sobrescreve a função da classe mãe
+    public boolean withdraw(double value) {
+        double valueToWithdraw = value + 0.2;
+        return super.withdraw(valueToWithdraw);
+    }
 }
