839affdfa261b02ef31e92b281ee66e2e0cdfb16
Gabriela Hermenegildo Junior <gabriela.hermenegil@meta.com.br>
2022-10-18 12:00:30 -0300

    adding get and delete

--- parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ parking-control/src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -5,6 +5,9 @@ import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
@@ -22,5 +25,17 @@ public class ParkingSpotService {
 
     public boolean existsByLicensePlateCar(String licensePlateCar) {
         return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+    }
+
+    public List<ParkingSpotModel> findAll() {
+        return parkingSpotRepository.findAll();
+    }
+
+    public Optional<ParkingSpotModel> findById(UUID id) {
+        return parkingSpotRepository.findById(id);
+    }
+
+    public void delete(ParkingSpotModel parkingSpotModel) {
+       parkingSpotRepository.delete(parkingSpotModel);
     }
 }
